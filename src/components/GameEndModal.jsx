643b1f75a62c3791b830d238c47157ce9b1f4100
Sean Ghods <137848705+seanghods@users.noreply.github.com>
2023-09-20 23:03:17 -0700

    Change final game over message

--- src/components/GameEndModal.jsx
+++ src/components/GameEndModal.jsx
@@ -3,14 +3,19 @@ import '../css/GameEndModal.css';
 export default function GameEnd({ isWinner, restart }) {
   return (
     <div className={isWinner ? 'game-over winner' : 'game-over loser'}>
-      <h3>Game Over!</h3>
       {isWinner ? (
-        <p>
-          Congrats! You won by catching all Pokemon without repeating a single
-          one!
-        </p>
+        <>
+          <h3>Congrats!</h3>
+          <p>
+            Congrats! You won by catching all Pokemon without repeating a single
+            one!
+          </p>
+        </>
       ) : (
-        <p>You failed to catch a unique Pokemon with that last catch!</p>
+        <>
+          <h3>Game Over!</h3>
+          <p>You failed to catch a unique Pokemon with that last catch!</p>
+        </>
       )}
       <button onClick={restart}>Restart!</button>
     </div>
